7bb7280e3c9734d51969c8c183784b7cb99eb939
FernandaSpineli <fernandaspinx@gmail.com>
2022-11-16 11:26:24 -0300

    criando teste unitario ListagemAgendamentos

--- agenda/tests.py
+++ agenda/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+import json
+from rest_framework.test import APITestCase
 
 # Create your tests here.
+class TestListagemAgendamentos(APITestCase):
+    def test_listagem_vazia(self):
+        response = self.client.get('/api/agendamentos/')
+        data = json.loads(response.content)
+        self.assertEqual(data, [])
+        
+        # json.dumps => transforma de python para json
+        # json.loads => trasforma de json para python
