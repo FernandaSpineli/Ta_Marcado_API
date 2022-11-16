2e79b2881224bdc543072fb7beaaead8c81409e2
FernandaSpineli <fernandaspinx@gmail.com>
2022-11-15 22:12:34 -0300

    removendo métodos que não estão em uso

--- agenda/serializers.py
+++ agenda/serializers.py
@@ -20,19 +20,3 @@ class AgendamentoSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError('E-mail brasileiro deve estarassociado a um número do Brasil (+55)')
         return attrs
     
-    def create(self, validated_data):
-        agendamento = Agendamento.objects.create(
-                data_horario=validated_data['data_horario'],
-                nome_cliente=validated_data['nome_cliente'],
-                email_cliente=validated_data['email_cliente'],
-                telefone_cliente=validated_data['telefone_cliente'],
-            )
-        return agendamento
-    
-    def update(self, instance, validated_data):
-        instance.data_horario = validated_data.get('data_horario', instance.data_horario)
-        instance.nome_cliente = validated_data.get('nome_cliente', instance.nome_cliente)
-        instance.email_cliente = validated_data.get('email_cliente', instance.email_cliente)
-        instance.telefone_cliente = validated_data.get('telefone_cliente', instance.telefone_cliente)
-        instance.save()
-        return instance
