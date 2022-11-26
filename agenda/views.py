3afb1765892101f51459160004b23c8ca5bfe070
FernandaSpineli <fernandaspinx@gmail.com>
2022-11-25 21:42:53 -0300

    atualizando views usando generics

--- agenda/views.py
+++ agenda/views.py
@@ -1,46 +1,19 @@
 from datetime import datetime
 
-from django.shortcuts import get_object_or_404
-from django.http import JsonResponse
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework import mixins, generics
 
 from agenda.models import Agendamento
 from agenda.serializers import AgendamentoSerializer
 
-class AgendamentoList(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    generics.GenericAPIView
-    ):
+class AgendamentoList(generics.ListCreateAPIView):
     queryset = Agendamento.objects.all()
     serializer_class = AgendamentoSerializer
+
     
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-    
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-    
-class AgendamentoDetail(
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    generics.GenericAPIView
-    ):
+class AgendamentoDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Agendamento.objects.all()
-    serializer_class = AgendamentoSerializer
-    
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-    
-    def patch(self, request, *args, **kwargs):
-        return self.partial_update(request, *args, **kwargs)
-    
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)        
+    serializer_class = AgendamentoSerializer     
 
 @api_view()
 def get_horarios(request):
