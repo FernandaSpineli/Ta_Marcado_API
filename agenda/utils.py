034ad2ff438f643f9a20f8a1d963ad68d34bd3af
FernandaSpineli <fernandaspinx@gmail.com>
2022-11-30 14:06:15 -0300

    atualizando método get_horarios

--- agenda/utils.py
+++ agenda/utils.py
@@ -1,9 +1,17 @@
 from typing import Iterable
 from datetime import date, datetime, timedelta, timezone
+import requests
 
 from agenda.models import Agendamento
+from agenda.libs import brasil_api
 
 def get_horarios_disponiveis(data:date) -> Iterable[datetime]:
+    try:
+        if brasil_api.is_feriado(data):
+            return []
+    except ValueError:
+        ...
+        
     start = datetime(year=data.year, month=data.month, day=data.day, hour=9, minute=0, tzinfo=timezone.utc)
     end = datetime(year=data.year, month=data.month, day=data.day, hour=18, minute=0, tzinfo=timezone.utc) 
     delta = timedelta(minutes=30)
