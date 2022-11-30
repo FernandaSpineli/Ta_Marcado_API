49d5bdb7a5847bfd5017489bd25e8e05b6f9c8ab
FernandaSpineli <fernandaspinx@gmail.com>
2022-11-30 13:34:37 -0300

    atualizando serializer

--- agenda/serializers.py
+++ agenda/serializers.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 from agenda.models import Agendamento
+from agenda.utils import get_horarios_disponiveis
 
 class AgendamentoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,10 +17,20 @@ class AgendamentoSerializer(serializers.ModelSerializer):
         except User.DoesNotExist:
             raise serializers.ValidationError('username não existe.')
         return prestador
-        
+       
+    def validate_data_horario(self, value):
+        if value < timezone.now():
+            raise serializers.ValidationError('Agendamento não pode ser feito no passado.')
+        if value not in get_horarios_disponiveis(value.date()):
+            raise serializers.ValidationError('Esse horário não está disponivel.')
+        return value
+     
     def validate(self, attrs):
         telefone_cliente = attrs.get('telefone_cliente', '')
         email_cliente = attrs.get('email_cliente', '')
+        data = attrs.get('data_horario').date()
+        if Agendamento.objects.filter(email_cliente=email_cliente, data_horario__date=data).exists():
+            raise serializers.ValidationError('Já existe um agendamento para esse cliente nessa data.')
         
         if email_cliente.endswith('.br') and telefone_cliente.startswith('+') and not telefone_cliente.startswith('+55'):
             raise serializers.ValidationError('E-mail brasileiro deve estarassociado a um número do Brasil (+55)')
